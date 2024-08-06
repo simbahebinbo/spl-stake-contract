053a1fe79ed3eac0cb4a0388c0bc92bc91698885
simbahebinbo <lansheng228@163.com>
2024-08-06 21:03:39 +0800

    init

--- programs/spl-stake/tests/simulate_test.rs
+++ programs/spl-stake/tests/simulate_test.rs
@@ -1,58 +1,105 @@
-// use anchor_lang::prelude::*;
-// use anchor_lang::solana_program::system_program;
-// use anchor_spl::token::{self, Mint, Token, TokenAccount};
-//
-// #[tokio::test]
-// async fn test_simulate() {
-//     let program_id = Pubkey::new_unique();
-//     let payer = Pubkey::new_unique();
-//     let mint = Pubkey::new_unique();
-//     let token_account = Pubkey::new_unique();
-//
-//     // 初始化上下文
-//     let mut program = ProgramTest::new(
-//         "my_program",
-//         program_id,
-//         processor!(my_program::my_instruction),
-//     );
-//
-//     program.add_account(
-//         mint,
-//         Account {
-//             lamports: 1000000,
-//             data: vec![0; Mint::LEN],
-//             owner: token::ID,
-//             executable: false,
-//             rent_epoch: 0,
-//         },
-//     );
-//
-//     program.add_account(
-//         token_account,
-//         Account {
-//             lamports: 1000000,
-//             data: vec![0; TokenAccount::LEN],
-//             owner: token::ID,
-//             executable: false,
-//             rent_epoch: 0,
-//         },
-//     );
-//
-//     let (mut banks_client, payer, recent_blockhash) = program.start().await;
-//
-//     let tx = Transaction::new_signed_with_payer(
-//         &[instruction::my_instruction(
-//             program_id,
-//             payer.pubkey(),
-//             token::ID,
-//             mint,
-//             token_account,
-//         )],
-//         Some(&payer.pubkey()),
-//         &[&payer],
-//         recent_blockhash,
-//     );
-//
-//     banks_client.process_transaction(tx).await.unwrap();
-// }
-//
+use anchor_lang::InstructionData;
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, spl_token, TokenAccount};
+use solana_program::instruction::Instruction;
+use solana_program_test::ProgramTest;
+use solana_sdk::account::Account;
+use solana_sdk::signature::Signer;
+use solana_sdk::transaction::Transaction;
+
+#[tokio::test]
+async fn test_simulate() {
+    let program_id = spl_stake::ID;
+    let mint = Pubkey::new_unique();
+    let token_account = Pubkey::new_unique();
+
+    // 初始化上下文
+    let mut program = ProgramTest::new(
+        "spl_stake",
+        program_id,
+        None,
+    );
+
+    // 添加mint账户
+    program.add_account(
+        mint,
+        Account {
+            lamports: 1000000000000,
+            data: vec![0; Mint::LEN],
+            owner: token::ID,
+            executable: false,
+            rent_epoch: 0,
+        },
+    );
+
+    // 添加token账户
+    program.add_account(
+        token_account,
+        Account {
+            lamports: 10000000000000,
+            data: vec![0; TokenAccount::LEN],
+            owner: token::ID,
+            executable: false,
+            rent_epoch: 0,
+        },
+    );
+
+    let (mut banks_client, payer, recent_blockhash) = program.start().await;
+
+    // 创建并发送初始化 mint 的指令
+    let init_mint_ix = spl_token::instruction::initialize_mint(
+        &token::ID,
+        &mint,
+        &payer.pubkey(),
+        None,
+        0, // decimals
+    ).unwrap();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[init_mint_ix],
+        Some(&payer.pubkey()),
+        &[&payer],
+        recent_blockhash,
+    );
+
+    banks_client.process_transaction(tx).await.unwrap();
+    
+    // 创建并发送初始化 token account 的指令
+    let init_token_account_ix = spl_token::instruction::initialize_account(
+        &token::ID,
+        &token_account,
+        &mint,
+        &payer.pubkey(),
+    ).unwrap();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[init_token_account_ix],
+        Some(&payer.pubkey()),
+        &[&payer],
+        recent_blockhash,
+    );
+
+    banks_client.process_transaction(tx).await.unwrap();
+
+    let simulate_ix = Instruction {
+        program_id: program_id,
+        accounts: spl_stake::accounts::Simulate {
+            payer: payer.pubkey(),
+            token_program: token::ID,
+            token_account: token_account,
+            mint: mint,
+        }
+            .to_account_metas(None),
+        data: spl_stake::instruction::Simulate {}.data(),
+    };
+
+    let simulate_tx = Transaction::new_signed_with_payer(
+        &[simulate_ix],
+        Some(&payer.pubkey()),
+        &[&payer],
+        recent_blockhash,
+    );
+
+    banks_client.process_transaction(simulate_tx).await.unwrap();
+}
+
